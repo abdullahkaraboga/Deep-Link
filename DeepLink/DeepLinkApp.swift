54ca76073dcc823f5edcd22dcdc9360b62e7eb4a
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-04-06 23:42:10 +0300

    Initial Commit

---
+++ DeepLink/DeepLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLinkApp.swift
+//  DeepLink
+//
+//  Created by Abdullah Karaboğa on 6.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeepLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
